6068f17d7a79ea85cb1368f8621a5ab6ebce3044
Heryck325 <79487827+Heryck325@users.noreply.github.com>
2022-09-07 20:07:53 -0400

    Delete tiposPrimitivos.js

--- tiposPrimitivos.js
+++
@@ -1,5 +0,0 @@
-let nome = 'Rafael'; //string literal
-let idade = 25;// number literal
-let estaAparovado = true; // boolean
-let sobreNome = undefined; // Undefined
-let corSelecionada = null; //Redefinir um valor
